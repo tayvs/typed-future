036a1a3c04dfa0c3767e91e7042173196bf90c09
tayvs <>
2023-11-22 00:30:33 +0200

    reimplement some methods

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,15 +9,24 @@ object Main extends App {
 
   case class YourError(t: Throwable) extends Throwable(t)
 
-  val _: TypedFuture[Int, MyError] = TypedFuture.successful[MyError].apply(21)
+  val _: TypedFuture[Int, MyError] = TypedFuture.successful[MyError](21)
   val _: TypedFuture[Int, MyError] = TypedFuture.failed[Int](MyError(new Exception("")))
+  val _: TypedFuture[Int, MyError] = TypedFuture[MyError](Future.successful(12))
 
   val r: TypedFuture[String, MyError] = Future
     .successful(12)
     .withExpectedError[IllegalArgumentException]
     .mapError(MyError(_))
     .map(_ + 1)
-    .flatMap(i => TypedFuture.successful[MyError].apply(i.toString))
+    .flatMap(i => TypedFuture.successful[MyError](i.toString))
     .flatMap(i => TypedFuture.failed[String](YourError(new Exception(""))))
     .flatMap(i => TypedFuture.failed[String](MyError(new Exception(""))))
+
+
+  val _: Future[Int] = Future
+    .successful(12)
+    .recover { case t => 21 }
+
+
+//  Thread.sleep(1000)
 }
--- src/main/scala/TypedFuture.scala
+++ src/main/scala/TypedFuture.scala
@@ -1,51 +1,58 @@
+import TypedFuture.TypedFutureConstructor
+
 import scala.concurrent.{ExecutionContext, Future}
 import scala.reflect.ClassTag
 import scala.util.Try
 
-class TypedFuture[T, +E <: Throwable: ClassTag] private[TypedFuture] (fut: Future[T]) extends AnyVal {
+class TypedFuture[T, +E <: Throwable : ClassTag] private(val fut: Future[T]) /*extends AnyVal*/ {
 
-   def failed: Future[Throwable] = fut.failed
-  // TODO:
-  //    fut.recoverWith { case e: E => TypedFuture.successful[E, Throwable](e) }(ExecutionContext.parasitic)
+  def failed: Future[Throwable] =
+    fut.failed.flatMap {
+      case e: E => Future.successful(e)
+      case t => Future.failed(t)
+    }(ExecutionContext.parasitic)
 
-   def foreach[U](f: T => U)(implicit executor: ExecutionContext): Unit = fut.foreach(f)
+  def foreach[U](f: T => U)(implicit executor: ExecutionContext): Unit = fut.foreach(f)
 
-   def transform[S](s: T => S, f: Throwable => Throwable)(implicit executor: ExecutionContext) = fut.transform(s, f)
+  def transform[S, E1 <: Throwable : ClassTag](s: T => S, f: E => E1)(implicit executor: ExecutionContext): TypedFuture[S, E1] =
+    this.map(s).mapError(f)
 
-   def map[S](f: T => S)(implicit executor: ExecutionContext): TypedFuture[S, E] = new TypedFuture(fut.map(f))
+  def map[S](f: T => S)(implicit executor: ExecutionContext): TypedFuture[S, E] = new TypedFuture(fut.map(f))
 
-   def flatMap[S](f: T => Future[S])(implicit executor: ExecutionContext) = fut.flatMap(f)
+  def flatMap[S](f: T => Future[S])(implicit executor: ExecutionContext): TypedFuture[S, Throwable] =
+    fut.flatMap(f).withExpectedError[Throwable]
 
-   def flatten[S](implicit ev: T <:< Future[S]) = fut.flatten
+  //  def flatten[S](implicit ev: T <:< Future[S]) = fut.flatten
 
-   def filter(p: T => Boolean)(implicit executor: ExecutionContext) = fut.filter(p)
+  //  def filter(p: T => Boolean)(implicit executor: ExecutionContext) = fut.filter(p)
 
-   def collect[S](pf: PartialFunction[T, S])(implicit executor: ExecutionContext) = fut.collect(pf)
+  //  def collect[S](pf: PartialFunction[T, S])(implicit executor: ExecutionContext) = fut.collect(pf)
 
-   def recover[U >: T](pf: PartialFunction[Throwable, U])(implicit executor: ExecutionContext) = fut.recover(pf)
+  def recover[U >: T](pf: PartialFunction[E, U])(implicit executor: ExecutionContext) =
+    new TypedFuture[U, E](fut.recover { case e: E if pf.isDefinedAt(e) => pf(e) })
 
-   def recoverWith[U >: T](pf: PartialFunction[Throwable, Future[U]])(implicit executor: ExecutionContext) = fut.recoverWith(pf)
+  //  def recoverWith[U >: T, E1 <: Throwable : ClassTag](pf: PartialFunction[E, TypedFuture[U, E1]])(implicit executor: ExecutionContext) = fut.recoverWith(pf)
 
-   def zip[U](that: Future[U]) = fut.zip(that)
+//  def zip[U](that: Future[U]) = fut.zip(that)
 
-   def zipWith[U, R](that: Future[U])(f: (T, U) => R)(implicit executor: ExecutionContext) = fut.zipWith(that)(f)
+//  def zipWith[U, R](that: Future[U])(f: (T, U) => R)(implicit executor: ExecutionContext) = fut.zipWith(that)(f)
 
-   def fallbackTo[U >: T](that: Future[U]) = fut.fallbackTo(that)
+//  def fallbackTo[U >: T](that: Future[U]) = fut.fallbackTo(that)
 
-   def mapTo[S](implicit tag: ClassTag[S]) = fut.mapTo
+  def mapTo[S](implicit tag: ClassTag[S]): TypedFuture[S, E] = new TypedFuture(fut.mapTo[S])
 
-   def andThen[U](pf: PartialFunction[Try[T], U])(implicit executor: ExecutionContext) = fut.andThen(pf)
+  def andThen[U](pf: PartialFunction[Try[T], U])(implicit executor: ExecutionContext) = fut.andThen(pf)
 
-   def onComplete[U](f: Try[T] => U)(implicit executor: ExecutionContext): Unit = fut.onComplete(f)
+  def onComplete[U](f: Try[T] => U)(implicit executor: ExecutionContext): Unit = fut.onComplete(f)
 
-   def transform[S](f: Try[T] => Try[S])(implicit executor: ExecutionContext) = fut.transform(f)
+//  def transform[S](f: Try[T] => Try[S])(implicit executor: ExecutionContext) = fut.transform(f)
+//
+//  def transformWith[S](f: Try[T] => Future[S])(implicit executor: ExecutionContext) = fut.transformWith(f)
 
-   def transformWith[S](f: Try[T] => Future[S])(implicit executor: ExecutionContext) = fut.transformWith(f)
+  def flatMap[T1, E1 <: Throwable : ClassTag](f: T => TypedFuture[T1, E1])(implicit executor: ExecutionContext): TypedFuture[T1, E1] =
+    new TypedFuture[T1, E1](fut.flatMap(f(_)).withExpectedError[E1])
 
-  def flatMap[T1, E1 <: Throwable](f: T => TypedFuture[T1, E1])(implicit executor: ExecutionContext): TypedFuture[T1, E1] =
-    new TypedFuture[T1, E1](fut.flatMap(f))
-
-  def mapError[E1 <: Throwable: ClassTag](f: E => E1)(implicit executor: ExecutionContext): TypedFuture[T, E1] =
+  def mapError[E1 <: Throwable : ClassTag](f: E => E1)(implicit executor: ExecutionContext): TypedFuture[T, E1] =
     new TypedFuture[T, E1](fut.recoverWith { case t: E => Future.failed(f(t)) })
 
 }
@@ -54,30 +61,34 @@ object TypedFuture {
 
   implicit def typed2Vanilla[T, E <: Throwable](tf: TypedFuture[T, E]): Future[T] = tf.fut
 
-  class Successful[E <: Throwable /*: ClassTag*/ ] extends Any {
+  class Successful[E <: Throwable /*: ClassTag*/ ] extends AnyRef {
     def apply[T](v: T)(implicit ct: ClassTag[E]): TypedFuture[T, E] = new TypedFuture[T, E](Future.successful(v))
   }
 
-  class Failed[T] extends Any {
-    def apply[E <: Throwable: ClassTag](e: E): TypedFuture[T, E] = new TypedFuture[T, E](Future.failed(e))
+  class Failed[T] extends AnyRef {
+    def apply[E <: Throwable : ClassTag](e: E): TypedFuture[T, E] = new TypedFuture[T, E](Future.failed(e))
+  }
+
+  class Apply[E <: Throwable /*: ClassTag*/ ] extends AnyRef {
+    def apply[T](f: Future[T])(implicit ct: ClassTag[E]): TypedFuture[T, E] = new TypedFuture[T, E](f)
   }
 
   implicit class TypedFutureConstructor[T](val fut: Future[T]) extends AnyVal {
-    def withExpectedError[E <: Throwable: ClassTag]: TypedFuture[T, E] = new TypedFuture[T, E](fut)
+    def withExpectedError[E <: Throwable : ClassTag]: TypedFuture[T, E] = new TypedFuture[T, E](fut)
   }
 
-  def apply[T, E <: Throwable: ClassTag](fut: Future[T]): TypedFuture[T, E] = new TypedFuture[T, E](fut)
+  //  def apply[T, E <: Throwable: ClassTag](fut: Future[T]): TypedFuture[T, E] = new TypedFuture[T, E](fut)
+  def apply[E <: Throwable /*: ClassTag*/ ] = new Apply[E]
 
   //  def successful[T, E <: Throwable: ClassTag](value: T): TypedFuture[T, E] = new TypedFuture[T, E](Future.successful(value))
-  def successful[E <: Throwable: ClassTag]: Successful[E] = new Successful[E]
+  def successful[E <: Throwable /*: ClassTag*/ ]: Successful[E] = new Successful[E]
+
   //  def failed[E <: Throwable: ClassTag, T](error: E): TypedFuture[T, E] = new TypedFuture[T, E](Future.failed(error))
   def failed[T]: Failed[T] = new Failed[T]
 
-  def fromEither[E <: Throwable: ClassTag, T](e: Either[E, T]): TypedFuture[T, E] = e match {
-    case Left(err)  => TypedFuture.failed[T](err)
+  def fromEither[E <: Throwable : ClassTag, T](e: Either[E, T]): TypedFuture[T, E] = e match {
+    case Left(err) => TypedFuture.failed[T](err)
     case Right(v) => TypedFuture.successful[E].apply(v)
   }
-
-
 
 }
